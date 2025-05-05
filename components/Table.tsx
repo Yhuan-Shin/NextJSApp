8fbb0c99a70cddf70b23b231b2dd1d66c1e6db04
Yhuan Shin <tejima911@gmail.com>
2025-05-05 09:53:42 +0800

    feat: implement data fetching from Firebase and display items in table

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,7 +1,34 @@
+'use client';
 import { Button } from "@/components/ui/button"
 import AddModal from "./crud/AddModal";
-export default function Table() {
+import { db } from "@/app/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
+async function fetchData() {
+    const querySnapshot = await getDocs(collection(db, "items"));
+
+    const data: { id: string; [key: string]: any }[] = [];
+    querySnapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+    });
+    return data;
+}
+
+
+export default function Table() {
+    const[itemData, setData] = useState<{ id: string; [key: string]: any }[]>([]);
+    
+    useEffect(() => {
+        const fetchDataFromFirebase = async () => {
+            const data = await fetchData();
+            setData(data);
+        }
+        fetchDataFromFirebase();
+    }
+    , []);
+
+    
     return (
         <div>
 
@@ -18,16 +45,18 @@ export default function Table() {
                     </tr>
                 </thead>
                 <tbody className="bg-gray-100">
-                    <tr className="border-b border-gray-200 hover:bg-gray-50">
-                        <td className="px-4 py-2">1</td>
-                        <td className="px-4 py-2">Item 1</td>
-                        <td className="px-4 py-2">Description of Item 1</td>
-                        <td className="px-4 py-2">10</td>
-                        <td className="px-4 py-2 flex space-x-2">
-                            <Button className="bg-blue-500 text-white px-4 py-2 rounded">Edit</Button>
-                            <Button className="bg-red-500 text-white px-4 py-2 rounded ml-2">Delete</Button>
-                        </td>
-                    </tr>
+                    {itemData.map((data) => (
+                        <tr key={data.id} className="border-b border-gray-200 hover:bg-gray-50">
+                            <td className="px-4 py-2">{data.id}</td>
+                            <td className="px-4 py-2">{data.itemName}</td>
+                            <td className="px-4 py-2">{data.description}</td>
+                            <td className="px-4 py-2">{data.quantity}</td>
+                            <td className="px-4 py-2 flex gap-2">
+                                <Button variant="outline" size="sm" className="mr-2">Edit</Button>
+                                <Button variant="destructive" size="sm">Delete</Button>
+                            </td>
+                        </tr>
+                    ))}
                    
 
                 </tbody>
